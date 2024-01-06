e785eddd0c5ac59ba30b6265798180905a0076b1
Felix <felix@blueinnotechnology.com>
2024-01-06 14:45:26 +0800

    I am submitting

--- main.py
+++ main.py
@@ -3,7 +3,7 @@
 Q1: 
 '''
 
-x = None    # TODO: Replace `None` with your answer
+x = 10    # TODO: Replace `None` with your answer
 
 print('Q1:')
 print(f'x = {x}')
@@ -13,9 +13,9 @@ print('#' * 30)
 Q2:
 '''
 
-a = None    # TODO: Replace `None` with your answer
-b = None    # TODO: Replace `None` with your answer
-c = None    # TODO: Replace `None` with your answer
+a = 6    # TODO: Replace `None` with your answer
+b = 9    # TODO: Replace `None` with your answer
+c = 16    # TODO: Replace `None` with your answer
 
 print('Q2:')
 print(f'a = {a}')
@@ -27,8 +27,8 @@ print('#' * 30)
 Q3:
 '''
 
-first = None    # TODO: Replace `None` with your answer
-second = None   # TODO: Replace `None` with your answer
+first = 19    # TODO: Replace `None` with your answer
+second = 8   # TODO: Replace `None` with your answer
 
 print('Q3:')
 print(f'first = {first}')
@@ -39,20 +39,57 @@ print('#' * 30)
 Q4:
 '''
 
-# TODO: Write your code here
+first = 8
+second = 19
+
+first += second         # Shortened
+second = first - second
+first -= second         # Shortened
 
 ##################################################
 '''
 Q5:
 '''
 
-# TODO: Write your code here
+work_hours = 4 + 5 + 8 + 4
+hourly_salary = 8.75
+tax = 0.2
+
+# Calculate pay at work based on hours worked each day
+print("My total hours worked:")
+print(work_hours)
+
+print("My hourly salary:")
+print(f"${hourly_salary}")
+
+print("My total pay:")
+print(work_hours * hourly_salary)
+
+print("My taxes owed:")  # 20% tax
+print(work_hours * hourly_salary * tax)
 
 ##################################################
 '''
 Q6:
 '''
 
-# TODO: Write your code here
+# This program computes the total amount owed for a meal,
+# assuming 8% tax and a 15% tip.
+
+subtotal = 38 + 40 + 30
+tax = 0.08
+tip = 0.15
+
+print("Subtotal:")
+print(subtotal)
+
+print("Tax:")
+print((subtotal) * tax)
+
+print("Tip:")
+print((subtotal) * tip)
+
+print("Total:")
+print(subtotal + subtotal * tax + subtotal * tip)
 
 ##################################################
